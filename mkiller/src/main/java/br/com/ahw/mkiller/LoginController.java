dcc3aa677189c35bf55d0c25feb6963fb7f1d20d
Fernando Sapata <fernandosapata@gmail.com>
2016-06-20 23:23:44 -0300

    Testes de Redirect para JSP

--- mkiller/src/main/java/br/com/ahw/mkiller/LoginController.java
+++ mkiller/src/main/java/br/com/ahw/mkiller/LoginController.java
@@ -1,21 +1,15 @@
 package br.com.ahw.mkiller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@EnableAutoConfiguration
 public class LoginController {
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(LoginController.class, args);
+	
+    @RequestMapping("/login")
+//    @ResponseBody
+    public String login() {
+        return "login";
     }
 }
---
+++ mkiller/src/main/java/br/com/ahw/mkiller/SpringBootWebApplication.java
@@ -0,0 +1,14 @@
+package br.com.ahw.mkiller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpringBootWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+}
